ea31a7af7998a8cb339a591c37b9626d9b89553d
Yash Khare <khareyash05@gmail.com>
2025-01-24 11:39:01 +0530

    rem

--- main.py
+++ main.py
@@ -23,18 +23,3 @@ class Features(BaseModel):
     num_bedrooms: float
     population: float
     address: str
-
-@app.post("/predict")
-def predict(features: Features):
-    features_dict = {
-        "Avg. Area Income": [features.income],
-        "Avg. Area House Age": [features.house_age],
-        "Avg. Area Number of Rooms": [features.num_rooms],
-        "Avg. Area Number of Bedrooms": [features.num_bedrooms],
-        "Area Population": [features.population],
-        "Address": [features.address]
-    }
-    
-    features_df = pd.DataFrame(features_dict)
-    prediction = predict_price(features_df)
-    return {"price": prediction[0]}    

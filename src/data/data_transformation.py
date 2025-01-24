e6cd5012f78f18d9f634f75b5aec61b29ccfcf01
Yash Khare <khareyash05@gmail.com>
2025-01-24 12:21:21 +0530

    check

--- src/data/data_transformation.py
+++ src/data/data_transformation.py
@@ -20,6 +20,7 @@ class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
 
+    def initialize_data_transformation(self,train_path,test_path):
         try:
             train_df=pd.read_csv(train_path)
             test_df=pd.read_csv(test_path)
